ba94885a3991d957aacbae762000ad9e6d25422d
Jayon Kim <jpkim921@gmail.com>
2018-09-02 16:51:01 +0000

    automatically backed up by learn

--- db/migrate/20180902164912_change_password_digest_to_be_string_in_users.rb
+++ db/migrate/20180902164912_change_password_digest_to_be_string_in_users.rb
@@ -1,5 +1,5 @@
 class ChangePasswordDigestToBeStringInUsers < ActiveRecord::Migration[5.2]
   def change
-    change_cplumn :users, :password_digest, :string
+    change_column :users, :password_digest, :string
   end
 end
